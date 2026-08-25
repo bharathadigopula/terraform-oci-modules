d145c91950e67eac3ec0ea0537a790aa109c88d9
bharath <adigopulabharath@outlook.com>
2026-08-25 13:17:54 +0530

    feat: add OCI budget alert module

---
+++ budget-alert/versions.tf
@@ -0,0 +1,14 @@
+#==============================================================================
+# TERRAFORM AND PROVIDER REQUIREMENTS
+#==============================================================================
+
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "8.28.0"
+    }
+  }
+}

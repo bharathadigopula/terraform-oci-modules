58c17f2eaea391cb6ac45986caf3e36075a6abaf
bharath <adigopulabharath@outlook.com>
2026-08-31 14:10:51 +0530

    ci: add Jenkins module validation pipeline

---
+++ .jenkins/pipelines/validate.groovy
@@ -0,0 +1,29 @@
+//==============================================================================
+// TERRAFORM MODULE VALIDATION
+//==============================================================================
+
+@Library('jenkins-pipeline-templates@v1.2.0') _
+
+repositoryValidationPipeline(
+    terraformDirectories: [
+        'bastion',
+        'budget-alert',
+        'cloudflare-access-tunnel',
+        'compute-instance',
+        'core-network-security-group',
+        'core-network-security-group-security-rule',
+        'core-security-list',
+        'core-subnet',
+        'identity-compartment',
+        'identity-dynamic-group',
+        'identity-policy',
+        'kms-key',
+        'kms-vault',
+        'mysql-db-system',
+        'object-storage-bucket',
+        'random-password',
+        'vault-secret',
+        'vcn'
+    ],
+    validateWorkflows: true
+)

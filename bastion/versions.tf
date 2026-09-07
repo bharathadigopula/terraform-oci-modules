b4396cca67fa1d612dbf7b5b1c8db5485cb6567d
bharathadigopula <48708941+bharathadigopula@users.noreply.github.com>
2026-09-07 18:22:49 +0530

    feat: support OCI provider 9 across modules (#23)

--- bastion/versions.tf
+++ bastion/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "8.28.0"
+      version = "9.0.0"
     }
   }
 }
--- mysql-db-system/versions.tf
+++ mysql-db-system/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">= 8.28.0, < 9.0.0"
+      version = ">= 9.0.0, < 10.0.0"
     }
   }
 }

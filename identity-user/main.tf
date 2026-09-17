9bb9947faa2b418f65db0b693b30556d9c68d4a3
bharath <adigopulabharath@outlook.com>
2026-09-17 13:49:18 +0530

    fix: require OCI identity user primary email

--- identity-user/main.tf
+++ identity-user/main.tf
@@ -5,6 +5,7 @@
 resource "oci_identity_user" "this" {
   compartment_id = var.tenancy_id
   description    = var.description
+  email          = var.email
   name           = var.name
 
   freeform_tags = var.freeform_tags
